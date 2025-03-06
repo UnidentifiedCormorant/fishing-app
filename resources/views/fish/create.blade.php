cf0300dc97ef092e5f31c810049c8a4b5c8b34c3
UnidentifiedCormorant <uusers1350@gmail.com>
2025-03-06 15:41:40 +0700

    feature(fish): Добавлен вывод сообщений об ошибках в формах создания и редактирования

--- resources/views/fish/create.blade.php
+++ resources/views/fish/create.blade.php
@@ -1,15 +1,20 @@
 <form action="{{route('fish.store')}}" method="POST">
     @csrf
+
     <label for="title">Название</label><br>
+    @error('title') <div style="color: #ef4444">{{$message}}</div> @enderror
     <input type="text" id="title" name="title"><br>
 
     <label for="size">Размер</label><br>
+    @error('size') <div style="color: #ef4444">{{$message}}</div> @enderror
     <input type="number" step=0.01 id="size" name="size"><br>
 
     <label for="weight">Вес</label><br>
+    @error('weight') <div style="color: #ef4444">{{$message}}</div> @enderror
     <input type="number" step=0.01 id="weight" name="weight"><br>
 
     <label for="rating">Редкость</label><br>
+    @error('rating') <div style="color: #ef4444">{{$message}}</div> @enderror
     <select name="rating" id="rating">
         @foreach(\App\Domain\Enums\Fish\RatingEnum::toList() as $key => $value)
             <option value="{{$key}}">{{$value}}</option>
--- resources/views/fish/edit.blade.php
+++ resources/views/fish/edit.blade.php
@@ -1,16 +1,21 @@
 <form action="{{route('fish.update', ['fish' => $fish])}}" method="POST">
     @csrf
     @method('PATCH')
+
     <label for="title">Название</label><br>
+    @error('title') <div style="color: #ef4444">{{$message}}</div> @enderror
     <input type="text" id="title" name="title" value="{{$fish->title}}"><br>
 
     <label for="size">Размер</label><br>
+    @error('size') <div style="color: #ef4444">{{$message}}</div> @enderror
     <input type="text" step=0.01 id="size" name="size" value="{{$fish->size}}"><br>
 
     <label for="weight">Вес</label><br>
+    @error('weight') <div style="color: #ef4444">{{$message}}</div> @enderror
     <input type="text" step=0.01 id="weight" name="weight" value="{{$fish->weight}}"><br>
 
     <label for="rating">Редкость</label><br>
+    @error('rating') <div style="color: #ef4444">{{$message}}</div> @enderror
     <select name="rating" id="rating">
         @foreach(\App\Domain\Enums\Fish\RatingEnum::toList() as $key => $value)
             <option value="{{$key}}" @if($key === $fish->rating) selected="selected" @endif>{{$value}}</option>
